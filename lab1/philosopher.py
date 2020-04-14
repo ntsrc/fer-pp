faedc099bbf481e0899d0313fd5e3b8bee58a17a
ante <ante.saric3@fer.hr>
2020-04-14 18:38:28 +0200

    ...

--- lab1/philosopher.py
+++ lab1/philosopher.py
@@ -17,28 +17,28 @@ class Tag(IntEnum):
 
 comm = MPI.COMM_WORLD
 
-num_phils = comm.Get_size()
-if (num_phils < 2):
+numPhils = comm.Get_size()
+if (numPhils < 2):
     sys.exit("There must be at least 2 philosophers at the table")
 
-my_rank = comm.Get_rank()
-left_rank = (my_rank - 1 + num_phils) % num_phils
-right_rank = (my_rank + 1) % num_phils
+myRank = comm.Get_rank()
+leftRank = (myRank - 1 + numPhils) % numPhils
+rightRank = (myRank + 1) % numPhils
 
-forks = {left_rank : Fork(my_rank == 0, False, False), right_rank : Fork(my_rank != (num_phils - 1), False, False)}
+forks = {leftRank : Fork(myRank == 0, False, False), rightRank : Fork(myRank != (numPhils - 1), False, False)}
 
-def print_msg(msg):
-    print('|'.join(['{:^26}'.format(msg if r == my_rank else '') for r in range(num_phils)]), flush = True)
+def printMsg(msg):
+    print('|'.join(['{:^26}'.format(msg if r == myRank else '') for r in range(numPhils)]), flush = True)
 
-def send_fork(rank):
+def sendFork(rank):
     forks[rank].clean = True
-    print_msg("Sending fork to " + str(rank))
+    printMsg("Sending fork to " + str(rank))
     comm.send([], dest = rank, tag = Tag.SEND_FORK)
     forks[rank].here = False
 
 def think():
     thinking = random.randint(2, 6)
-    print_msg("I'm thinking...")
+    printMsg("I'm thinking...")
 
     while thinking:
         time.sleep(1)
@@ -47,21 +47,21 @@ def think():
         if comm.Iprobe(tag = Tag.REQUEST_FORK, status = status):
             rank = status.Get_source()
             comm.recv(source = rank, tag = Tag.REQUEST_FORK)
-            send_fork(rank)
+            sendFork(rank)
 
         thinking -= 1
 
-    print_msg('Done thinking')
+    printMsg('Done thinking')
 
-def acquire_forks():
-    print_msg("I'm hungry")
+def acquireForks():
+    printMsg("I'm hungry")
 
     for rank in forks:
         if not forks[rank].here:
-            print_msg('Requesting fork from ' + str(rank))
+            printMsg('Requesting fork from ' + str(rank))
             comm.send([], dest = rank, tag = Tag.REQUEST_FORK)
 
-    while not forks[left_rank].here or not forks[right_rank].here:
+    while not forks[leftRank].here or not forks[rightRank].here:
         status = MPI.Status()
         if comm.Iprobe(status = status):
             rank = status.Get_source()
@@ -75,40 +75,40 @@ def acquire_forks():
 
 def eat():
     eating = random.randint(2,6)
-    print_msg("I'm eating...")
+    printMsg("I'm eating...")
 
     time.sleep(eating)
 
     for rank in forks:
         forks[rank].clean = False
 
-    print_msg("Done eating") 
+    printMsg("Done eating") 
 
-def handle_requests():
+def handleRequests():
     for rank in forks:
         if forks[rank].requested:
-            send_fork(rank)
+            sendFork(rank)
             forks[rank].requested = False
 
-def signal_handler(sig, frame):
+def signalHandler(sig, frame):
     sys.exit(0)
 
 def main():
-    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGINT, signalHandler)
 
-    random.seed(int(time.time()) ^ (my_rank << left_rank))
+    random.seed(int(time.time()) ^ (myRank << leftRank))
 
-    if my_rank == 0:
-        print('|'.join(['{:^26}'.format('Philosopher {}'.format(r)) for r in range(num_phils)]) + '\n' + ('-'*(27*num_phils-1)), flush = True)
+    if myRank == 0:
+        print('|'.join(['{:^26}'.format('Philosopher {}'.format(r)) for r in range(numPhils)]) + '\n' + ('-' * (27 * numPhils - 1)), flush = True)
     comm.Barrier()
 
     while True:
         think() 
-        acquire_forks()
+        acquireForks()
         eat()
-        handle_requests() 
+        handleRequests() 
 
-        print_msg('-'*26)
+        printMsg('-' * 26)
 
 if __name__ == "__main__":
     main()
